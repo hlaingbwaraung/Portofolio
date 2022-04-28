d20210bb57df5d0689df91c9745cc3c442195dff
hbanihon <mizbwar@gmail.com>
2022-04-29 01:24:55 +0900

    1st commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+     
+         <PostsList :posts="posts"></PostsList>
+       
+    
+  </div>
+</template>
+
+<script>
+import PostsList from '../components/PostsList'
+import { ref } from 'vue'
+export default {
+  components: { PostsList },
+    setup(){
+    
+      let posts=ref([
+      
+      ])
+      return {posts};
+    }
+}
+</script>
